f31a88bcabcf49419df2118806d6cd66d8795f3c
mehadiHasanDiner <mehadi.cosmo@gmail.com>
2023-11-11 02:35:56 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const port = 5000;
+
+const chefs = require("./data/chef.json");
+
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.get("/chefs", (req, res) => {
+  res.send(chefs);
+});
+
+app.get("/chef/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  console.log(id);
+  const selectedChef = chefs.find((chef) => chef.id === id);
+  res.send(selectedChef);
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}!`);
+});
